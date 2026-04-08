fbe92186952b8aea7431f88379a0291a52c5523f
ekasbedi613 <ekasbedi613@gmail.com>
2026-04-08 12:10:45 -0600

    module 4

---
+++ operators.py
@@ -0,0 +1,9 @@
+a=78
+b=80
+c=100
+d=41
+e=85
+sum=a+b+c+d+e
+print("the total sum is ",sum)
+avg=sum/5
+print("the average is ",avg)
---
+++ operators2.py
@@ -0,0 +1,10 @@
+amount=int(input("enter your amount"))
+a1=amount//100
+a2=(amount%100)//50
+a3=((amount%100)%50)//10
+print("100$ notes",a1)
+print("50$ notes",a2)
+print("10$ notes",a3)
+
+print("arithmetic operators : +,-,*,/,%,//,**")
+print("comparisons operators: ==,!=,<,>,<=,<=")
